a8208bd541bb86ef0d5db57c54cc9ec24a9ff723
Ganesh Lokare <lokaregns@gmail.com>
2022-12-30 09:11:34 +0530

    prediction updated

--- movies/constants/data_ingestion_constants.py
+++ movies/constants/data_ingestion_constants.py
@@ -1,5 +1,5 @@
-start_index = 16101
-end_index = 16201
+start_index = 16201
+end_index = 17001
 movie_data_path = "movies/artifacts/data_access/movies_data.csv"
 keywords_data_path = "movies/artifacts/data_access/keywords_data.csv"
 credits_data_path = "movies/artifacts/data_access/credits_data.csv"
--- movies/prediction.py
+++ movies/prediction.py
@@ -4,15 +4,16 @@ import sys
 from movies.constants.data_ingestion_constants import preprocessed_data_path, cosine_sim_data_path
 from movies.exception import MovieException
 
-
+new_df = pd.read_csv(preprocessed_data_path)
 class Prediction:
     def __init__():
         pass
+        
+    
 
     def get_recommendations(movie):
         try:
 
-            new_df = pd.read_csv(preprocessed_data_path)
             cosine_sim = load(cosine_sim_data_path)
 
             # get the index of title
@@ -28,4 +29,43 @@ class Prediction:
         except  Exception as e:
                 raise  MovieException(e,sys)
 
+
+    def High_Rated_Movies():
+        try:
+            C= new_df['vote_average'].mean() # mean vote across the whole report
+            m= new_df['vote_count'].quantile(0.9) # minimum votes required to be listed in the chart.
+            q_movies = new_df.copy().loc[new_df['vote_count'] >= m] # filter out the movies that qualify for the chart
+
+            def weighted_rating(x, m=m, C=C):
+                v = x['vote_count']
+                R = x['vote_average']
+                # Calculation based on the IMDB formula
+                return (v/(v+m) * R) + (m/(m+v) * C)
+            
+            q_movies['score'] = q_movies.apply(weighted_rating, axis=1)
+            #Sort movies based on score calculated above
+            q_movies = q_movies.sort_values('score', ascending=False)
+            #Print the top 10 movies
+            print(q_movies[['title', 'vote_count', 'vote_average', 'score']].head(10))
+        except  Exception as e:
+                raise  MovieException(e,sys)
+
+    def Popular_Movies():
+        try: 
+            pop= new_df.sort_values('popularity', ascending=False)
+            print(pop['title'].head(6))
+        except  Exception as e:
+                raise  MovieException(e,sys)
+
+    def Latest_Movies():
+        try:
+            latest= new_df.sort_values('release_date', ascending=False)
+            print(latest[['title','release_date']].head(6))
+        except  Exception as e:
+                raise  MovieException(e,sys)  
+
+
 Prediction.get_recommendations('Iron Man')
+Prediction.High_Rated_Movies()
+Prediction.Popular_Movies()
+Prediction.Latest_Movies()
