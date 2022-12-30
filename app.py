8619612a8ad4ae9020cd0d5e38bc2ed6c2893278
Ganesh Lokare <lokaregns@gmail.com>
2022-12-30 09:55:56 +0530

    templates added

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from flask import Flask,request, jsonify, render_template
+import pandas as pd
+from numpy import load
+import sys
+from movies.constants.data_ingestion_constants import preprocessed_data_path, cosine_sim_data_path
+from movies.exception import MovieException
+
+new_df = pd.read_csv(preprocessed_data_path)
 
 app = Flask(__name__)
 
@@ -6,7 +13,22 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/movie_recommendation',methods=['POST'])
 
+def get_recommendations(movie):
+    cosine_sim = load(cosine_sim_data_path)
+
+    # get the index of title
+    index = new_df[new_df['title'] == movie].index[0] 
+            
+    # Get the pairwsie similarity scores of all movies with that movie and Sort the movies based on the similarity scores
+    distances = sorted(list(enumerate(cosine_sim[index])),reverse=True,key = lambda x: x[1])
+            
+    # Get the scores of the 5 most similar movies
+    for i in distances[1:6]:
+        print(new_df.iloc[i[0]].title)
+
+        
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8080)
