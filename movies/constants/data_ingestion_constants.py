533350aa1fbe258a3d6cf5f652e2fee487111a01
Ganesh Lokare <lokaregns@gmail.com>
2023-01-04 15:16:25 +0530

    auto fethching done

--- movies/constants/data_ingestion_constants.py
+++ movies/constants/data_ingestion_constants.py
@@ -1,5 +1,5 @@
-start_index = 16201
-end_index = 16301
+start_index = 16311
+end_index = 16321
 movie_data_path = "movies/artifacts/data_access/movies_data.csv"
 keywords_data_path = "movies/artifacts/data_access/keywords_data.csv"
 credits_data_path = "movies/artifacts/data_access/credits_data.csv"
